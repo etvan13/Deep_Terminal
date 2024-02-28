0d9f6f2126e73599eba0ea4c9721a80c9c28282c
evan3660 <etvan13@tamu.edu>
2024-02-28 13:24:22 -0600

    Terminal changes

---
+++ DilationClasses.py
@@ -0,0 +1,52 @@
+#-------Sub classes for Dilation--------#
+
+#--------------------------Inevitable Progression---------------------------#
+class OneDDemo:
+    def run(self):
+        print("One D Demo")
+        input("> Temp")
+        return "exit_to_main"
+    
+
+class TwoDDemo:
+    def run(self):
+        print("Two D Demo")
+        input("> Temp")
+        return "exit_to_main"
+    
+
+class ThreeDDemo:
+    def run(self):
+        print("Three D Demo")
+        input("> Temp")
+        return "exit_to_main"
+    
+
+class GraphView:
+    def run(self):
+        print("Graph Demo")
+        input("> Temp")
+        return "exit_to_main"
+        
+
+#--------------------------Spacetime Dilation---------------------------#
+    
+
+class TimeDilation:
+    def run(self):
+        print("Time D Demo")
+        return "exit_to_main"
+
+
+class ComplexObserver:
+    def run(self):
+        print("Complex Demo")
+        input("> Temp")
+        return "exit_to_main"
+    
+
+class UnknownFunctionality:
+    def run(self):
+        print("Unknown Demo")
+        input("> Temp")
+        return "exit_to_main"
